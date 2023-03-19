d5b1e861deddcb23f6566e37e180846dce75bc7d
arildohp <118690657+arildohp@users.noreply.github.com>
2023-03-19 12:57:24 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 $(document).ready(function(e) {
-
-$('form button').click(function(e){
+$("form button").click(function(e){
    e.preventDefault();
-   const NovaTarefa= $('#lista-tarefa').val();
-   const NovoItem= $('<li></li>');
+   const NovaTarefa = $("#lista-tarefa").val();
+   const NovoItem = $("<li></li>");
    $(`<p>${NovaTarefa}</p>`).appendTo(NovoItem);
-   
-    $('<p></p>').click(function(e){
-      $('form').slideUp();
-  })
 
-   $(NovoItem).appendTo('ul');
-   $('#lista-tarefa').val('');
-})
+   $(NovoItem).appendTo("ul");
 
 
-})
+   $("#lista-tarefa").val("");
+   $(NovoItem).on("click", function () {
+      $(NovoItem) .addclass("tarefa-feita");
+   });
+});
+
+});
